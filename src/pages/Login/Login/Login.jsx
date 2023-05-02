ab5b989a9a80fd6819635bd5ddf5496afc488d11
rashedrk <rashedrk001@gmail.com>
2023-05-02 22:53:40 +0600

    login page added

---
+++ src/pages/Login/Login/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col ">
+                <div className="text-center mb-5">
+                    <h1 className="text-5xl font-bold">Login <span className='text-[#E6272C]'> Here!</span></h1>
+                </div>
+                <div className="card  w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body w-96">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="Enter Your Email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="Enter Your Password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-2">
+                            <button className="btn bg-red-600 hover:bg-red-500 border-none">Login</button>
+                        </div>
+                        <p><small>Don't Have an Account? <Link className='link link-error' to='/register'>Create Account</Link></small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
