701027c7425ee73a1bc2e53f94441ff366f59276
rashedrk <rashedrk001@gmail.com>
2023-05-02 23:01:50 +0600

    registration page added

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h2>This is register</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col ">
+                    <div className="text-center mb-5">
+                        <h1 className="text-5xl font-bold">Please <span className='text-[#E6272C]'> Register </span> Here!</h1>
+                    </div>
+                    <div className="card  w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body w-96">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="Enter Your Name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo</span>
+                                </label>
+                                <input type="text" placeholder="Enter Your Picture" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="Enter Your Email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="Enter Your Password" className="input input-bordered" />
+
+                            </div>
+                            <div className="form-control mt-2">
+                                <button className="btn bg-red-600 hover:bg-red-500 border-none">Register</button>
+                            </div>
+                            <label className='label'>
+                                <span><small>Already Have an Account? <Link className='link link-error' to='/register'>Login Here</Link></small></span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
