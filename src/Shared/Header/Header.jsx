f44ac32d9d0eda0ba06e2f1ec0b7fe889e5f4311
rashedrk <rashedrk001@gmail.com>
2023-05-04 19:53:51 +0600

    added active route

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../Providers/AuthProvider/AuthProvider';
+import './Header.css'
 
 const Header = () => {
-    const { user,logOut } = useContext(AuthContext);
+    const { user, logOut } = useContext(AuthContext);
 
     //handle logout button click
     const handleLogOut = () => {
         logOut()
-        .then(() => {
-            console.log('success full logout');
-        })
-        .catch(err => console.log(err))
+            .then(() => {
+                console.log('success full logout');
+            })
+            .catch(err => console.log(err))
     }
     {
         console.log(user?.photoURL);
@@ -25,18 +26,18 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link>Home</Link></li>
-                        <li><Link>Blog</Link></li>
-                        <li><Link>About Us</Link></li>
+                        <li><NavLink to='/'>Home</NavLink></li>
+                        <li><NavLink to='/blog'>Blog</NavLink></li>
+                        <li><NavLink to='/about'>About Us</NavLink></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">Dish<span className='text-[#E6272C]'> Discoveries</span></a>
+                <Link to='/' className="btn btn-ghost normal-case text-xl">Dish<span className='text-[#E6272C]'> Discoveries</span></Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link>Home</Link></li>
-                    <li><Link>Blog</Link></li>
-                    <li><Link>About Us</Link></li>
+                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li><NavLink to='/blog'>Blog</NavLink></li>
+                    <li><NavLink to='/about'>About Us</NavLink></li>
                 </ul>
             </div>
             <div className="navbar-end">
@@ -48,7 +49,7 @@ const Header = () => {
                                     <div className="w-10 rounded-full">
                                         <img src={user.photoURL} />
                                     </div>
-                                    
+
                                 </div>
                             </div>
                             <button onClick={handleLogOut} className='ms-3 btn btn-outline btn-secondary btn-sm'>Log Out</button>
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            blog
+            <h2 className='text-xl font-semibold'>Tell us the differences between uncontrolled and controlled components.</h2>
+            <p className='mb-5'>ans: </p>
+            <h2 className='text-xl font-semibold'>How to validate React props using PropTypes
+            </h2>
+            <p className='mb-5'>ans: </p>
+            <h2 className='text-xl font-semibold'>Tell us the difference between nodejs and express js.</h2>
+            <p className='mb-5'>ans: </p>
+            <h2 className='text-xl font-semibold'>What is a custom hook, and why will you create a custom hook?</h2>
+            <p className='mb-5'>ans: </p>
         </div>
     );
 };
