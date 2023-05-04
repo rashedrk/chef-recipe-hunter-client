46c5d0145a6a53dadef8d948a8d2ed60cfe0cf01
rashedrk <rashedrk001@gmail.com>
2023-05-04 19:09:23 +0600

    404 page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,8 @@ import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login/Login";
 import Register from "../pages/Login/Register/Register";
 import ChefRecipes from "../pages/ChefRecipes/ChefRecipes/ChefRecipes";
+import Error from "../pages/Error/Error";
+import Blog from "../pages/Blog/Blog";
 
 const router = createBrowserRouter([
     {
@@ -26,6 +28,14 @@ const router = createBrowserRouter([
                 path: '/chef/:id',
                 element: <ChefRecipes></ChefRecipes>,
                 loader: ({params}) => fetch(`http://localhost:3000/chef/${params.id}`)
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '*',
+                element: <Error></Error>
             }
         ]
     }
---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <img src="https://marlincs.com/wp-content/uploads/2022/08/AdobeStock_212741283-1-scaled.jpeg" alt="" />
+        </div>
+    );
+};
+
+export default Error;
